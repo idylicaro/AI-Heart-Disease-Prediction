4a98f4dfc98095ed9015ec165dc937809b5faa60
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-11-17 16:34:23 -0300

    implementing df concat

--- application/main.py
+++ application/main.py
@@ -15,45 +15,15 @@ def evaluate(json):
     # load the model from disk
     classifier_mlp = pickle.load(open('mlp_model.sav', 'rb'))
     data = pd.read_json('test.json', orient='records')
-    df = pd.json_normalize(data['data'])
+    user_df = pd.json_normalize(data['data'])
     data = pd.read_csv('heart.csv')
-    data.append(df)
-    x, _ = get_predictors_by_user_json(df.values)
+    dfs = [data, user_df]
+    df = pd.concat(dfs, ignore_index=True)
+    x, _ = get_from_csv(df)
 
     # predict_proba return [probability for '0', probability for '1']
-    result = classifier_mlp.predict_proba(x[1].reshape(1, -1))
+    result = classifier_mlp.predict_proba(x[918].reshape(1, -1))
     return f'A probabilidade de você ter algum problema no coração é de {"{:.2f}".format(result[0][1] * 100)}%.'
-
-
-def get_predictors_by_user_json(predictors_user):
-    data = pd.read_csv('heart.csv')
-    predictors = data.iloc[:, 0:11].values
-    predictors.append(predictors_user)
-    escaler = preprocessing.StandardScaler()
-    escalerLabel = preprocessing.LabelBinarizer()
-
-    age = escaler.fit_transform(np.array(predictors[:, 0]).reshape(-1, 1))
-    escalerLabel.fit(['M', 'F'])
-    sex = escalerLabel.transform(np.array(predictors[:, 1]).reshape(-1, 1))
-    escalerLabel.fit(['ATA', 'NAP', 'ASY', 'TA'])
-    chestPainType = escalerLabel.transform(np.array(predictors[:, 2]).reshape(-1, 1))
-    restingBP = escaler.fit_transform(np.array(predictors[:, 3]).reshape(-1, 1))
-    cholesterol = escaler.fit_transform(np.array(predictors[:, 4]).reshape(-1, 1))
-    fastingBS = escaler.fit_transform(np.array(predictors[:, 5]).reshape(-1, 1))
-    escalerLabel.fit(['Normal', 'ST', 'LVH'])
-    restingECG = escalerLabel.transform(np.array(predictors[:, 6]).reshape(-1, 1))
-    maxHR = escaler.fit_transform(np.array(predictors[:, 7]).reshape(-1, 1))
-    escalerLabel.fit(['N', 'Y'])
-    exerciseAngina = escalerLabel.fit_transform(np.array(predictors[:, 8]).reshape(-1, 1))
-    oldpeak = escaler.fit_transform(np.array(predictors[:, 9]).reshape(-1, 1))
-    escalerLabel.fit(['Up', 'Flat', 'Down'])
-    sT_Slope = escalerLabel.transform(np.array(predictors[:, 10]).reshape(-1, 1))
-
-    predictors = np.column_stack((age, sex, chestPainType, restingBP, cholesterol, fastingBS, restingECG, maxHR,
-                                  exerciseAngina, oldpeak, sT_Slope))
-
-    return predictors
-
 
 def get_from_csv(data):
     """ Return predictors and targets from a single csv """
@@ -127,9 +97,8 @@ print('\n==MLP==')
 # save the model to disk
 filename = 'mlp_model.sav'
 pickle.dump(train_model("heart.csv", classifier_mlp), open(filename, 'wb'))
-# print(read_coef_file('weight.json'))
-evaluate(None)
-
+result = evaluate(None)
+print(result)
 print('\n')
 
 classifier_ppn = Perceptron(max_iter=5000, verbose=False, n_iter_no_change=10)
